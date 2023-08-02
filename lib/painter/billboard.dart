d5dcf3213245a7ad3ab7dcacc79a8856d6f6ed8e
nsenji <jerrysyre@gmail.com>
2023-08-02 13:06:57 +0300

    yes page design going well

---
+++ lib/painter/billboard.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomLine extends StatelessWidget {
+  const CustomLine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _CustomLinePainter(),
+      size: Size(10, 20), // Adjust the size as needed
+    );
+  }
+}
+
+class _CustomLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = Colors.grey // Change the color as desired
+      ..strokeWidth = 2;
+
+    final double centerX = size.width / 2;
+    const double lineStartY = 0;
+    final double lineEndY = size.height; // Adjust the line length as needed
+
+    final Rect rectangle = Rect.fromCenter(
+      center: Offset(centerX, size.height - 5 / 2),
+      width: 10,
+      height: 5,
+    );
+
+
+    // Draw the line
+    canvas.drawLine(
+        Offset(centerX, lineStartY), Offset(centerX, lineEndY), paint);
+
+    // Draw the circle
+    //  canvas.drawCircle(Offset(centerX, size.height), circleRadius, paint);
+
+    canvas.drawRect(rectangle, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false; // Since this is a static drawing, we don't need to repaint
+  }
+}
--- lib/widgets/yes.page.dart
+++ lib/widgets/yes.page.dart
@@ -1,3 +1,4 @@
+import 'package:attendance/painter/billboard.dart';
 import 'package:attendance/widgets/phone.login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -23,14 +24,22 @@ class _YesState extends State<Yes> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
+    return SafeArea(
+      child: Scaffold(
+          body: Stack(children: [
+        Container(
+          height: 120,
+          width: double.maxFinite,
+          color: Colors.blue,
+        ),
+        Padding(
           padding:
               const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 70),
           child: Column(
             children: [
               Card(
+                elevation: 4,
+                margin: EdgeInsets.all(0),
                 child: Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Row(
@@ -39,35 +48,27 @@ class _YesState extends State<Yes> {
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children:  [
+                        children: [
                           Text(
                             'PF 3434',
-                            style: TextStyle(fontSize: 24,fontWeight: FontWeight.w700),
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w700),
                           ),
                           SizedBox(
-                            height: 5,
+                            height: 3,
                           ),
                           Text(
                             'Head Office',
-                            style: TextStyle(fontSize: 15,fontWeight: FontWeight.w300),
+                            style: TextStyle(
+                                fontSize: 13, fontWeight: FontWeight.w300),
                           ),
                           SizedBox(
-                            height: 10,
+                            height: 20,
                           ),
                           Text(
-                            '24 February 2023',
+                            'August 2023',
                             style: TextStyle(fontSize: 17, color: Colors.grey),
                           ),
-                          SizedBox(height: 40,),
-                          Row(
-                            children: [
-                              Column(
-                                children: [
-                                  
-                                ],
-                              )
-                            ],
-                          )
                         ],
                       ),
                       CircleAvatar(
@@ -79,36 +80,119 @@ class _YesState extends State<Yes> {
                   ),
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: CustomLine()),
+                  Expanded(child: CustomLine())
+                ],
+              ),
+              Card(
+                color: Color.fromARGB(255, 234, 246, 255),
+                elevation: 2,
+                margin: EdgeInsets.all(0),
+                child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Absent',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '4',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              )
+                            ],
+                          ),
+                          VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 1,
+                            indent: 9,
+                            endIndent: 9,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Attendance',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '21',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              )
+                            ],
+                          ),
+                          VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 1,
+                            indent: 9,
+                            endIndent: 9,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Overtime',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '2',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
               SizedBox(
-                height: 40,
+                height: 60,
               ),
               Expanded(child: DoneCheckin()),
-
-
-            SizedBox(height: 70,),
-
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green, // Change the text color here
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        10.0), // Optional: Customize the button's shape
+              SizedBox(
+                height: 70,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green, // Change the text color here
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          10.0), // Optional: Customize the button's shape
+                    ),
                   ),
-                ),
-                onPressed: () {
-                  _signOut();
-                  Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => PhoneLoginPage()),
-                      ((route) => false));
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Text('Done'),
-                )),
+                  onPressed: () {
+                    _signOut();
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PhoneLoginPage()),
+                        ((route) => false));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Text('Done'),
+                  )),
             ],
           ),
-        ));
+        ),
+      ])),
+    );
   }
 }
 
@@ -150,42 +234,86 @@ class DoneCheckin extends StatefulWidget {
 class _DoneCheckinState extends State<DoneCheckin> {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 4,
-          mainAxisSpacing: 4,
-          childAspectRatio: (220/150)
-         ),
-      children: const [
-        Card(
-          color: Colors.green,
-          child: SizedBox(
-            height: 100,
-            width: 100,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 6),
+          child: Text(
+            'Wed. 23 August, 23',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
-        Card(
-          color: Colors.green,
-          child: SizedBox(
-            height: 100,
-            width: 100,
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                childAspectRatio: (220 / 150)),
+            children: [
+              Card(
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 15,
+                              child: Image.asset(
+                                'assets/images/check-in.png',
+                                height: 17,
+                                width: 17,
+                              ),
+                              backgroundColor: Color.fromARGB(255, 234, 246, 255),
+                            ),
+                            SizedBox(width: 10,),
+                            Text('Check In',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 15),)
+                          ],
+                        ),
+                        SizedBox(height: 15,),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Text('08:34 am',style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20),),
+                        ),SizedBox(height:14),
+                        Text('On time',style: TextStyle(fontSize: 13,color: Colors.grey),)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.green,
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Card(
+                color: Colors.green,
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Card(
+                color: Colors.green,
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                ),
+              )
+            ],
           ),
         ),
-        Card(
-          color: Colors.green,
-          child: SizedBox(
-            height: 100,
-            width: 100,
-          ),
-        ),
-        Card(
-          color: Colors.green,
-          child: SizedBox(
-            height: 100,
-            width: 100,
-          ),
-        )
       ],
     );
   }
