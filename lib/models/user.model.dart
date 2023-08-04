0dae895a206df34ac8f951fa12d50d9e8e72281a
nsenji <jerrysyre@gmail.com>
2023-08-04 18:02:30 +0300

    created the user model

---
+++ lib/models/user.model.dart
@@ -0,0 +1,39 @@
+class User {
+  final String firstname;
+  final String lastname;
+  final String PF;
+  final String mobile_number;
+  final String email;
+
+  const User({
+    required this.lastname,
+    required this.PF,
+    required this.mobile_number,
+    required this.email,
+    required this.firstname,
+  });
+
+  static User fromMap(Map<dynamic, dynamic> data) {
+    return User(
+      firstname: data['firstname'] ,
+      lastname: data['lastname'],
+      PF: data['PF'],
+      mobile_number: data['mobile_number'],
+      email: data['email'],
+
+    );
+  }
+
+
+  String toString() {
+    return PF;
+  }
+
+
+/// Converts the [User] instance into a [Map] instance that can be
+  /// serialized to JSON.
+
+   Map<String, dynamic> toJson() => {
+        
+      };
+}
